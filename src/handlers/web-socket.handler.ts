6f87d6a6a3e458a266919f578e63daa397cbdd1e
e-rojas <ed.fullstack.dev@gmail.com>
2024-05-30 17:21:42 -0300

    web socket logic refactored to smaller classes

--- src/handlers/web-socket.handler.ts
+++ src/handlers/web-socket.handler.ts
@@ -7,63 +7,6 @@ interface RequestWithUser extends IncomingMessage {
     user?: Partial<User>
 }
 
-// class WebSocketHandler {
-//     private wss: WebSocketServer;
-//     private clients: WebSocket[] = [];
-
-//     constructor(server: HTTPServer) {
-//         this.wss = new WebSocketServer({ server, verifyClient: this.verifyClient.bind(this) });
-//         this.initializeWebSocketServer();
-//     }
-
-//     private async verifyClient(info: { req: RequestWithUser }, callback: (res: boolean, code: number, message: string) => void) {
-//         const protocols = info.req.headers['sec-websocket-protocol'];
-//         const token = info.req.headers['authorization'];
-//         const isProtocolAccepted = protocols && protocols.split(',').map((p: any) => p.trim()).includes('wdn-v1');
-//         try {
-//             if (!token) {
-//                 callback(false, 401, 'Unauthorized due to missing token');
-//                 return;
-//             }
-//             const user = await verifyToken(token);
-//             if (token && user && isProtocolAccepted) {
-//                 console.log('Connection authenticated');
-//                 info.req.user = user.toJSON();
-//                 callback(true, 200, 'Authorized');
-//             } else {
-//                 callback(false, 401, 'Unauthorized due to token');
-//             }
-//         } catch (error) {
-//             console.error('Token verification failed:', error);
-//             callback(false, 401, 'Unauthorized due to token');
-//         }
-//     }
-
-//     private initializeWebSocketServer() {
-//         this.wss.on('connection', this.handleConnection.bind(this));
-//     }
-
-//     private handleConnection(ws: WebSocket, req: RequestWithUser) {
-//         ws.send(`Welcome ${req.user?.name}!`);
-//         this.clients.push(ws);
-//         this.broadcastMessage(`${req.user?.name} has joined the chat`, ws);
-//         ws.on('message', (message: string) => {
-//             ws.send(`Hello, you sent to the ws -> ${message}!`);
-//         });
-//         ws.on('close', () => {
-//             this.clients = this.clients.filter((client) => client !== ws);
-//             console.log('Client disconnected');
-//         });
-//     }
-
-//     private broadcastMessage(message: string, exclude: WebSocket) {
-//         this.clients.forEach((client) => {
-//             if (client !== exclude && client.readyState === WebSocket.OPEN) {
-//                 client.send(message);
-//             }
-//         });
-//     }
-// }
 class WebSocketHandler {
     private wss: WebSocketServer;
     private clientManager: WebsocketClientManager;
